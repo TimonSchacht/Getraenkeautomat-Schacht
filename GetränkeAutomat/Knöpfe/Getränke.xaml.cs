d18361a157b567cf446fca05c23889112964d09c
TimonSchacht <timon.schacht123@gmail.com>
2024-06-20 06:06:28 +0200

    Add files via upload

---
+++ GetränkeAutomat/Knöpfe/Getränke.xaml.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+
+namespace GetränkeAutomat.Knöpfe
+{
+
+    public partial class Getränke : UserControl
+    {
+        private float GetränkePreis;
+        private string momentanesGeld;
+        private string EingegebenesGeld;
+
+        MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+
+
+
+        public Getränke()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
+
+            GetränkePreis = 1.99f;
+            EingegebenesGeld = mainWindow.tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(EingegebenesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            if (flMomentanesGeld >= GetränkePreis)
+            {
+                float neuesGeld = flMomentanesGeld - GetränkePreis;
+
+                mainWindow.tbEingezahltesGeld.Text = "Geld: 0€";
+                mainWindow.AuszahlSumme.Text = $"{neuesGeld}€";
+                mainWindow.tbGetränk.Text = "Danke für Ihren Einkauf! Viel Spaß mit Ihrer Cola.";
+                await Task.Delay(3000);
+                mainWindow.tbGetränk.Text = "";
+                mainWindow.AuszahlSumme.Text = "";
+            }
+            else
+            {
+                mainWindow.tbEingezahltesGeld.Text = "Bitte werfen Sie mehr Geld ein";
+                await Task.Delay(2000);
+                mainWindow.tbEingezahltesGeld.Text = EingegebenesGeld;
+            }
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            GetränkePreis = 1.99f;
+            EingegebenesGeld = mainWindow.tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(EingegebenesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            if (flMomentanesGeld >= GetränkePreis)
+            {
+                float neuesGeld = flMomentanesGeld - GetränkePreis;
+
+                mainWindow.tbEingezahltesGeld.Text = "Geld: 0€";
+                mainWindow.AuszahlSumme.Text = $"{neuesGeld}€";
+                mainWindow.tbGetränk.Text = "Danke für Ihren Einkauf! Viel Spaß mit Ihrer Fanta.";
+                await Task.Delay(3000);
+                mainWindow.tbGetränk.Text = "";
+                mainWindow.AuszahlSumme.Text = "";
+            }
+            else
+            {
+                mainWindow.tbEingezahltesGeld.Text = "Bitte werfen Sie mehr Geld ein";
+                await Task.Delay(2000);
+                mainWindow.tbEingezahltesGeld.Text = EingegebenesGeld;
+            }
+
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            GetränkePreis = 0.89f;
+            EingegebenesGeld = mainWindow.tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(EingegebenesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            if (flMomentanesGeld >= GetränkePreis)
+            {
+                float neuesGeld = flMomentanesGeld - GetränkePreis;
+
+                mainWindow.tbEingezahltesGeld.Text = "Geld: 0€";
+                mainWindow.AuszahlSumme.Text = $"{neuesGeld}€";
+                mainWindow.tbGetränk.Text = "Danke für Ihren Einkauf! Viel Spaß mit Ihrem Wasser.";
+                await Task.Delay(3000);
+                mainWindow.tbGetränk.Text = "";
+                mainWindow.AuszahlSumme.Text = "";
+            }
+            else
+            {
+                mainWindow.tbEingezahltesGeld.Text = "Bitte werfen Sie mehr Geld ein";
+                await Task.Delay(2000);
+                mainWindow.tbEingezahltesGeld.Text = EingegebenesGeld;
+            }
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+            GetränkePreis = 2.98f;
+            EingegebenesGeld = mainWindow.tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(EingegebenesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            if (flMomentanesGeld >= GetränkePreis)
+            {
+                float neuesGeld = flMomentanesGeld - GetränkePreis;
+
+                mainWindow.tbEingezahltesGeld.Text = "Geld: 0€";
+                mainWindow.AuszahlSumme.Text = $"{neuesGeld}€";
+                mainWindow.tbGetränk.Text = "Danke für Ihren Einkauf! Viel Spaß mit Ihrem Red Bull.";
+                await Task.Delay(3000);
+                mainWindow.tbGetränk.Text = "";
+                mainWindow.AuszahlSumme.Text = "";
+            }
+            else
+            {
+                mainWindow.tbEingezahltesGeld.Text = "Bitte werfen Sie mehr Geld ein";
+                await Task.Delay(2000);
+                mainWindow.tbEingezahltesGeld.Text = EingegebenesGeld;
+            }
+        }
+
+        private async void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+            GetränkePreis = 1.49f;
+            EingegebenesGeld = mainWindow.tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(EingegebenesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            if (flMomentanesGeld >= GetränkePreis)
+            {
+                float neuesGeld = flMomentanesGeld - GetränkePreis;
+
+                mainWindow.tbEingezahltesGeld.Text = "Geld: 0€";
+                mainWindow.AuszahlSumme.Text = $"{neuesGeld}€";
+                mainWindow.tbGetränk.Text = "Danke für Ihren Einkauf! Viel Spaß mit Ihrer Apfel Schorle.";
+                await Task.Delay(3000);
+                mainWindow.tbGetränk.Text = "";
+                mainWindow.AuszahlSumme.Text = "";
+            }
+            else
+            {
+                mainWindow.tbEingezahltesGeld.Text = "Bitte werfen Sie mehr Geld ein";
+                await Task.Delay(2000);
+                mainWindow.tbEingezahltesGeld.Text = EingegebenesGeld;
+            }
+        }
+
+        private async void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+            GetränkePreis = 0.89f;
+            EingegebenesGeld = mainWindow.tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(EingegebenesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            if (flMomentanesGeld >= GetränkePreis)
+            {
+                float neuesGeld = flMomentanesGeld - GetränkePreis;
+
+                mainWindow.tbEingezahltesGeld.Text = "Geld: 0€";
+                mainWindow.AuszahlSumme.Text = $"{neuesGeld}€";
+                mainWindow.tbGetränk.Text = "Danke für Ihren Einkauf! Viel Spaß mit Ihrem Eiskaffee.";
+                await Task.Delay(3000);
+                mainWindow.tbGetränk.Text = "";
+                mainWindow.AuszahlSumme.Text = "";
+            }
+            else
+            {
+                mainWindow.tbEingezahltesGeld.Text = "Bitte werfen Sie mehr Geld ein";
+                await Task.Delay(2000);
+                mainWindow.tbEingezahltesGeld.Text = EingegebenesGeld;
+            }
+        }
+    }
+}
+            
+    
+
---
+++ GetränkeAutomat/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace GetränkeAutomat
+{
+    public partial class MainWindow : Window
+    {
+
+        public string EingegebenesGeld;
+        public float GeldPlus;
+        public float flEingegebenesGeld;
+        public float flMomentanesGeld;
+        public string MomentanesGeld; 
+        public string ZusammengerechnetesGeld;
+        public float flneuesGeld;
+        public string strneuesGeld;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            EingegebenesGeld = GeldEingabe.Text;
+            MomentanesGeld = tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(MomentanesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            if (float.TryParse(EingegebenesGeld, out float GeldPlus))
+            {
+
+                flneuesGeld = flMomentanesGeld + GeldPlus;
+                strneuesGeld = $"Geld: {flneuesGeld:F2}€";
+                tbEingezahltesGeld.Text = strneuesGeld;
+                return;
+            }
+            else
+            {
+                ZusammengerechnetesGeld = tbEingezahltesGeld.Text;
+                tbEingezahltesGeld.Text = "ERROR";
+                await Task.Delay(800);
+                tbEingezahltesGeld.Text = ZusammengerechnetesGeld;
+                return;   
+            }
+        }
+
+        private async void Ausgeben_Click(object sender, RoutedEventArgs e)
+        {
+            EingegebenesGeld = GeldEingabe.Text;
+            MomentanesGeld = tbEingezahltesGeld.Text;
+
+            string floatGeld = Regex.Match(MomentanesGeld, @"[-+]?\d+(\,\d+)?").Value;
+            float.TryParse(floatGeld, out float flMomentanesGeld);
+
+            AuszahlSumme.Text = $"{flMomentanesGeld:F2}€";
+            await Task.Delay(800);
+            AuszahlSumme.Text = "";
+            tbEingezahltesGeld.Text = "Geld: 0€";
+            return;
+        }
+
+        private void Getränke_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+    }
+
